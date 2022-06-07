98af32113be808a35ffb478d491b71ede274ae6e
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2022-06-07 12:55:55 -0300

    refactor: remove vuetify

--- utils/common.ts
+++ utils/common.ts
@@ -1,1 +1,48 @@
 export const generateId = () => Math.floor(Math.random() * Math.floor(10000));
+
+export const getWorkouts = () => [
+  {
+    id: generateId(),
+    name: "Chest",
+    exercises: [
+      {
+        id: generateId(),
+        name: "Bench Press",
+        sets: "10",
+        reps: "10",
+        load: "10",
+        observation: "very good exercise",
+      },
+      {
+        id: generateId(),
+        name: "Incline Bench Press",
+        sets: "10",
+        reps: "10",
+        load: "10",
+        observation: "very good exercise",
+      },
+    ],
+  },
+  {
+    id: generateId(),
+    name: "Back",
+    exercises: [
+      {
+        id: generateId(),
+        name: "Deadlift",
+        sets: "10",
+        reps: "10",
+        load: "10",
+        observation: "very good exercise",
+      },
+      {
+        id: generateId(),
+        name: "Pull-up",
+        sets: "10",
+        reps: "10",
+        load: "10",
+        observation: "very good exercise",
+      },
+    ],
+  },
+];
