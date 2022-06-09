f787faa3d754fcf757b944a0202d051f9bbc31d9
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2022-06-08 22:07:11 -0300

    feat: add bulma css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,20 @@
 import { defineNuxtConfig } from "nuxt";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
-export default defineNuxtConfig({});
+export default defineNuxtConfig({
+  modules: ["@nuxtjs/bulma"],
+  build: {
+    /*
+     ** You can extend webpack config here
+     */
+    postcss: {
+      postcssOptions: {
+        preset: {
+          features: {
+            customProperties: false,
+          },
+        },
+      },
+    },
+  },
+});
